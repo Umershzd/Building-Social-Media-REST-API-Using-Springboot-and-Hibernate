08bb284cf2e90756672f201c9b8fc7169d56a67f
Umershzd <m.umer.shehzad@hotmail.com>
2024-08-06 07:05:10 +0500

    changes

---
+++ Restful-Api-for-SocialMedia/src/main/java/com/rest_api/Restful_Api_for_SocialMedia/RestfulApiForSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.rest_api.Restful_Api_for_SocialMedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiForSocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiForSocialMediaApplication.class, args);
+	}
+
+}
---
+++ Restful-Api-for-SocialMedia/src/test/java/com/rest_api/Restful_Api_for_SocialMedia/RestfulApiForSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest_api.Restful_Api_for_SocialMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiForSocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
